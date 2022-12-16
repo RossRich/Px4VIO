e6b068ee10adfa7e63c628bb5b9b5c263d574192
Ross Rich <RossRich@bk.ru>
2022-12-16 12:48:21 +0000

    fix: удалил mav_msgs из зависимости сборки

--- src/nodes/px4_realsense_bridge.cpp
+++ src/nodes/px4_realsense_bridge.cpp
@@ -8,8 +8,7 @@
 
 namespace bridge {
 
-PX4_Realsense_Bridge::PX4_Realsense_Bridge(const ros::NodeHandle &nh)
-    : nh_(nh) {
+PX4_Realsense_Bridge::PX4_Realsense_Bridge(const ros::NodeHandle &nh) : nh_(nh) {
 
   std::string inputTopic = "/camera_t265/odom/sample_throttled";
   if (!nh.getParam("/px4_realsense_bridge_node/input_topic", inputTopic)) {
@@ -17,13 +16,10 @@ PX4_Realsense_Bridge::PX4_Realsense_Bridge(const ros::NodeHandle &nh)
   }
 
   // initialize subscribers
-  odom_sub_ = nh_.subscribe<const nav_msgs::Odometry &>(
-      inputTopic.c_str(), 10, &PX4_Realsense_Bridge::odomCallback, this);
+  odom_sub_ = nh_.subscribe<const nav_msgs::Odometry &>(inputTopic.c_str(), 10, &PX4_Realsense_Bridge::odomCallback, this);
   // publishers
-  mavros_odom_pub_ =
-      nh_.advertise<nav_msgs::Odometry>("/mavros/odometry/out", 10);
-  mavros_system_status_pub_ =
-      nh_.advertise<mavros_msgs::CompanionProcessStatus>("/mavros/companion_process/status", 1);
+  mavros_odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/mavros/odometry/out", 10);
+  mavros_system_status_pub_ = nh_.advertise<mavros_msgs::CompanionProcessStatus>("/mavros/companion_process/status", 1);
 
   last_callback_time = ros::Time::now();
 
